d535e691af3e1a9900745260040b2d7d00779e27
Mitchell Martinez <you@example.com>
2023-11-14 19:40:17 -0500

    Modified CSS for Confirmation page

--- src/Components/Confirmation.js
+++ src/Components/Confirmation.js
@@ -1,34 +1,40 @@
 // Confirmation.js
 import React from 'react';
+import './index.css'; // Import the CSS file
 
 const Confirmation = () => {
-    const order = {
-        items: [
-          { name: 'Item 1', price: 10 },
-          { name: 'Item 2', price: 15 },
-          { name: 'Item 3', price: 20 },
-          { name: 'Item 4', price: 25 },
-          { name: 'Item 5', price: 30 },
-          { name: 'Item 6', price: 35 },
-          { name: 'Item 7', price: 40 },
-          { name: 'Item 8', price: 45 },
-        ],
-        totalAmount: 220,
-        status: 'Paid',
-      };  
+  const order = {
+    items: [
+      { name: 'Item 1', price: 10 },
+      { name: 'Item 2', price: 15 },
+      { name: 'Item 3', price: 20 },
+      { name: 'Item 4', price: 25 },
+      { name: 'Item 5', price: 30 },
+      { name: 'Item 6', price: 35 },
+      { name: 'Item 7', price: 40 },
+      { name: 'Item 8', price: 45 },
+    ],
+    totalAmount: 220,
+    status: 'Paid',
+  };
+
   return (
-    <div>
-      <h2>Order Confirmation</h2>
-      <p>Thank you for your order! Your order details are as follows:</p>
-      <ul>
+    <div className="confirmation-container">
+      <h2 className="confirmation-header">Order Confirmation</h2>
+      <p className="confirmation-text">
+        Thank you for your order! Your order details are as follows:
+      </p>
+      <ul className="confirmation-list">
         {order.items.map((item, index) => (
-          <li id="confirm-cart" key={index}>
+          <li className="confirmation-item" key={index}>
             {item.name} - ${item.price}
           </li>
         ))}
       </ul>
-      <p>Total Amount: ${order.totalAmount}</p>
-      <p>Status: {order.status}</p>
+      <p className="confirmation-total">Total Amount: ${order.totalAmount}</p>
+      <p className="confirmation-status">
+        Status: <span className="status-text">{order.status}</span>
+      </p>
     </div>
   );
 };
