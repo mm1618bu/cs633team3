55dc3816d8744f361e58e3a3d0bd7dff890a99f4
sujithbattu <sujithbattu007@gmail.com>
2023-12-05 18:44:04 -0500

    Adds McDonalds and Chiptole images and fixes card

--- src/Components/Chipotle.js
+++ src/Components/Chipotle.js
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import './index.css';
 import OrderShoppingCart from './OrderShoppingCart';
 import Reviews from './Reviews';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import burrito from '../img/burrito.png';
+import bowl from '../img/bowl.png';
+import tacos from '../img/tacos.png';
+import quesadilla from '../img/quesadilla.png';
+import chipsGuacamole from '../img/chips_guacamole.png';
+import salad from '../img/salad.png';
+import soda from '../img/Soda.png';
+import water from '../img/water.png';
 
 const foodItems = [
   {
@@ -9,53 +20,53 @@ const foodItems = [
     "description": "A classic burrito filled with your choice of protein, rice, beans, and toppings.",
     "price": 7.99,
     "calories": 700,
-    "image": "burrito.jpg"
+    "image": burrito
   },
   {
     "name": "Bowl",
     "description": "A bowl with your choice of protein, rice, beans, and toppings.",
     "price": 7.49,
     "calories": 600,
-    "image": "bowl.jpg"
+    "image": bowl
   },
   {
     "name": "Tacos (3-Pack)",
     "description": "Three soft or hard shell tacos with your choice of protein and toppings.",
     "price": 8.99,
     "calories": 450,
-    "image": "tacos.jpg"
+    "image": tacos
   },
   {
     "name": "Quesadilla",
     "description": "A quesadilla filled with your choice of protein, cheese, and toppings.",
     "price": 6.99,
     "calories": 550,
-    "image": "quesadilla.jpg"
+    "image":quesadilla
   },
   {
     "name": "Chips and Guacamole",
     "price": 3.49,
     "calories": 320,
-    "image": "chips_guacamole.jpg"
+    "image": chipsGuacamole
   },
   {
     "name": "Salad",
     "description": "A salad with your choice of protein, greens, and toppings.",
     "price": 8.49,
     "calories": 400,
-    "image": "salad.jpg"
+    "image": salad
   },
   {
     "name": "Soda",
     "price": 1.99,
     "calories": 150,
-    "image": "soda.jpg"
+    "image": soda
   },
   {
     "name": "Bottled Water",
     "price": 1.29,
     "calories": 0,
-    "image": "water.jpg"
+    "image": water
   }
 ];
 
@@ -106,6 +117,8 @@ const Chipotle = () => {
         {foodItems.map((item, index) => (
           <div key={index} className="food-card">
             <div className="food-info">
+              <Row>
+                <Col md={6}>
               <h2>{item.name}</h2>
               <p>{item.description}</p>
               <p>Price: ${item.price}</p>
@@ -119,8 +132,11 @@ const Chipotle = () => {
                   <button onClick={() => increaseQuantity(item)}>+</button>
                 </div>
               )}
+              </Col>
+                <Col md={6}>
+          <img src={item.image} alt={item.name} /></Col>
+              </Row>
             </div>
-            <img src={item.image} alt={item.name} />
           </div>
         ))}
       </div>
--- src/Components/McDonalds.js
+++ src/Components/McDonalds.js
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import './index.css';
 import OrderShoppingCart from './OrderShoppingCart';
 import Reviews from './Reviews';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Big_Mac from '../img/Big_Mac.png';
+import Quarter_Pounder from '../img/Quater_Pounder.png';
+import Chicken_Breast_Meat from '../img/Chicken_Breast_Meat.png';
+import Fench_Fries from '../img/Fench_Fries.png';
+import Apple_Slices from '../img/Apple_Slices.png';
+import Soda from '../img/Soda.png';
 
 const foodItems = [
     {
@@ -9,49 +18,49 @@ const foodItems = [
       "description": "The one and only McDonald's Big Mac.",
       "price": 4.99,
       "calories": 550,
-      "image": "Big_Mac.png"
+      "image": Big_Mac
     },
     {
       "name": "Quarter Pounder with Cheese",
       "price": 3.79,
       "calories": 520,
-      "image": "Quater_Pounder.png"
+      "image": Quarter_Pounder
     },
     {
       "name": "McChicken",
       "price": 2.29,
       "calories": 350,
-      "image": "Chicken_Breast_Meat.jpg"
+      "image": Chicken_Breast_Meat
     },
     {
       "name": "Spicy McChicken",
       "price": 2.49,
       "calories": 400,
-      "image": "Chicken_Breast_Meat.png"
+      "image": Chicken_Breast_Meat
     },
     {
       "name": "French Fries",
       "price": 1.89,
       "calories": 365,
-      "image": "Fench_Fries.png"
+      "image": Fench_Fries
     },
     {
       "name": "Apple Slices",
       "price": 1.00,
       "calories": 35,
-      "image": "Apple_Slices.png"
+      "image": Apple_Slices
     },
     {
       "name": "Coca-Cola",
       "price": 1.69,
       "calories": 140,
-      "image": "Soda.png"
+      "image": Soda
     },
     {
       "name": "Diet Coke",
       "price": 1.69,
       "calories": 0,
-      "image": "Soda.png"
+      "image": Soda
     }
   ];
   const McDonalds = () => {
@@ -99,6 +108,8 @@ const foodItems = [
         <div className="food-grid">
           {foodItems.map((item, index) => (
             <div key={index} className="food-card">
+              <Row>
+               <Col md={6}>
               <div className="food-info">
                 <h2>{item.name}</h2>
                 <p>{item.description}</p>
@@ -114,7 +125,11 @@ const foodItems = [
                   </div>
                 )}
               </div>
+              </Col>
+              <Col md={6}>
               <img src={item.image} alt={item.name} />
+              </Col>
+              </Row>
             </div>
           ))}
         </div>
