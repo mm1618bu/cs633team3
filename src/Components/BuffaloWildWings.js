8dbe81cec84726f7488bfbf7427a91bda62cff47
sujithbattu <sujithbattu007@gmail.com>
2023-12-05 18:53:18 -0500

    Adds buffalo wild wings images and fixes the food cards

--- src/Components/BuffaloWildWings.js
+++ src/Components/BuffaloWildWings.js
@@ -3,61 +3,72 @@ import './index.css';
 import OrderShoppingCart from './OrderShoppingCart';
 import Reviews from './Reviews';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import traditionalWings from '../img/traditional_wings.png';
+import bonelessWings from '../img/boneless_wings.png';
+import buffaloChickenNachos from '../img/buffalo_chicken_nachos.png';
+import chickenQuesadilla from '../img/chicken_quesadilla.png';
+import buffaloRanchWrap from '../img/buffalo_ranch_wrap.png';
+import mozzarellaSticks from '../img/mozzarella_sticks.png';
+import softDrinks from '../img/soft_drinks.png';
+
 const foodItems = [
   {
     "name": "Traditional Wings",
     "description": "Classic chicken wings with your choice of sauce.",
     "price": 10.99,
     "calories": 800,
-    "image": "traditional_wings.jpg"
+    "image": traditionalWings
   },
   {
     "name": "Boneless Wings",
     "description": "Boneless chicken wings with various sauce options.",
     "price": 9.99,
     "calories": 700,
-    "image": "boneless_wings.jpg"
+    "image": bonelessWings
   },
   {
     "name": "Buffalo Chicken Nachos",
     "description": "Crispy nachos topped with shredded buffalo chicken, cheese, and veggies.",
     "price": 12.49,
     "calories": 850,
-    "image": "buffalo_chicken_nachos.jpg"
+    "image": buffaloChickenNachos
   },
   {
     "name": "Chicken Quesadilla",
     "description": "Grilled chicken, cheese, and veggies in a warm tortilla.",
     "price": 8.99,
     "calories": 600,
-    "image": "chicken_quesadilla.jpg"
+    "image": chickenQuesadilla
   },
   {
     "name": "Buffalo Ranch Wrap",
     "description": "Grilled chicken, lettuce, tomatoes, and ranch dressing in a wrap.",
     "price": 11.99,
     "calories": 750,
-    "image": "buffalo_ranch_wrap.jpg"
+    "image": buffaloRanchWrap
   },
   {
     "name": "Mozzarella Sticks",
     "description": "Golden fried mozzarella sticks served with marinara sauce.",
     "price": 7.99,
     "calories": 450,
-    "image": "mozzarella_sticks.jpg"
+    "image": mozzarellaSticks
   },
   {
     "name": "Buffalo Wild Wings Sampler",
     "description": "A variety of wings, tenders, and mozzarella sticks with different sauces.",
     "price": 15.99,
     "calories": 1100,
-    "image": "bww_sampler.jpg"
+    "image": buffaloRanchWrap
   },
   {
     "name": "Soft Drinks",
     "price": 2.49,
     "calories": 150,
-    "image": "soft_drinks.jpg"
+    "image": softDrinks
   }
 ];
 
@@ -108,6 +119,8 @@ const BuffaloWildWings = () => {
         {foodItems.map((item, index) => (
           <div key={index} className="food-card">
             <div className="food-info">
+              <Row>
+                <Col md={6}>
               <h2>{item.name}</h2>
               <p>{item.description}</p>
               <p>Price: ${item.price}</p>
@@ -120,9 +133,11 @@ const BuffaloWildWings = () => {
                   <span>{orderItems.find((orderItem) => orderItem.name === item.name).quantity}</span>
                   <button onClick={() => increaseQuantity(item)}>+</button>
                 </div>
-              )}
+              )}</Col>
+                <Col md={6}>
+            <img src={item.image} alt={item.name} /></Col>
+              </Row>
             </div>
-            <img src={item.image} alt={item.name} />
           </div>
         ))}
       </div>
